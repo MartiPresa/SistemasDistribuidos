f52452962383c27b4470652a6b2a2eb47019c0b1
MartiPresa <102812269+MartiPresa@users.noreply.github.com>
2023-08-22 09:46:07 -0300

    Create ejer3-1

---
+++ javascripting/ejer3-1.js
@@ -0,0 +1,15 @@
+let x = 10; // Declaración de Número (entero)
+let y = 11.2 // Declaración de Número (decimal)
+
+let z = 'Hola mundo' // Declaración de cadena de texto
+console.log( typeof( x ) );
+console.log( typeof( y ) );
+console.log( typeof( z ) );
+
+console.log('Analice y ejecute este otro código');
+console.log( 7 + 7 + 7 );
+console.log( 7 + 7 + "7" );
+console.log( "7" + 7 + 7 );
+console.log( 10 - 2 - 4 );
+console.log( 10 - 2 - "4" );
+console.log( "10" - 2 - 4 );
